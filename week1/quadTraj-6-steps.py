af51ea1779321395f3b7352484277ef8c2f52448
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-05-06 23:42:16 +0530

    week 1 solutions

---
+++ week1/quadTraj-6-steps.py
@@ -0,0 +1,34 @@
+import quadrotor.command as cmd
+from math import sqrt
+
+def plan_mission(mission):
+
+    # this is an example illustrating the different motion commands,
+    # replace them with your own commands and activate all beacons
+    commands  = [
+        #cmd.down(0.5),
+        #cmd.right(1),
+        #cmd.turn_left(45),
+        #cmd.forward(sqrt(2)),
+        #cmd.turn_right(45),
+        #cmd.right(1),
+        #cmd.turn_left(45),
+        #cmd.forward(sqrt(0.5)),
+        #cmd.turn_left(90),
+        #cmd.forward(sqrt(0.5)),
+        #cmd.turn_left(45),
+        #cmd.forward(1),
+        #cmd.turn_right(45),
+        #cmd.backward(sqrt(2)),
+        #cmd.turn_left(45),
+        #cmd.forward(1),
+        cmd.up(1),
+        cmd.forward(5),
+        cmd.left(2),
+        cmd.backward(5),
+        cmd.right(4),
+        cmd.forward(5),
+        
+    ]
+
+    mission.add_commands(commands)
